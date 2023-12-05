b538007440609c939c1f87bd7a9f9747d9ad1970
usuyuki <63891531+usuyuki@users.noreply.github.com>
2023-12-05 16:41:23 +0900

    wip Arduino Time実装

--- src/lib/wasm-functions/arduino/arduino.cpp
+++ src/lib/wasm-functions/arduino/arduino.cpp
@@ -1,10 +1,33 @@
 #include <lib/wasm-functions/arduino/arduino.hpp>
 // m3ApiRawFunction はただのマクロなので注意
-m3ApiRawFunction(m3_arduino_delay)
+
+/**
+ * Time
+ */
+m3ApiRawFunction(m3_delay)
 {
     // cppcheck-suppress cstyleCast
     m3ApiGetArg(uint32_t, ms);
     delay(ms);
+    m3ApiSuccess();
+}
+m3ApiRawFunction(m3_delayMicroseconds)
+{
+    // cppcheck-suppress cstyleCast
+    m3ApiGetArg(uint32_t, us);
+    delayMicroseconds(us);
+    m3ApiSuccess();
+}
+m3ApiRawFunction(m3_micros)
+{
+    m3ApiReturnType(uint32_t);
+    m3ApiReturn(micros());
+    m3ApiSuccess();
+}
+m3ApiRawFunction(m3_millis)
+{
+    m3ApiReturnType(uint32_t);
+    m3ApiReturn(millis());
     m3ApiSuccess();
 }
 
@@ -13,7 +36,13 @@ M3Result mahiwa_LinkArduino(IM3Runtime runtime)
     IM3Module module = runtime->modules;
     const char *arduino = "arduino";
 
-    m3_LinkRawFunction(module, arduino, "delay", "v(i)", &m3_arduino_delay);
+    /**
+     * Time
+     */
+    m3_LinkRawFunction(module, arduino, "delay", "v(i)", &m3_delay);
+    m3_LinkRawFunction(module, arduino, "delayMicroseconds", "v(i)", &m3_delayMicroseconds);
+    m3_LinkRawFunction(module, arduino, "millis", "v(i)", &m3_millis);
+    m3_LinkRawFunction(module, arduino, "micros", "v(i)", &m3_micros);
 
     return m3Err_none;
 }
